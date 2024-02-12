ea8aa3ef89cd25b9928cd93206590667eb4d3cea
katebmedia <katebmedia@gmail.com>
2024-02-12 10:42:10 +0330

    feat: :tada: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ["*.{html,js}"],
+    theme: {
+        extend: {
+            boxShadow: {
+                '3xl': '0px 10px 50px 0px #8B8B8B1A',
+            },
+            colors: {
+              'primary': '#FC8517',
+              'primary-light': '#FED1A8',
+              'secondary': '#083870',
+              'secondary': '#083870',
+              'info': '#0AE5CA',
+              'stone-550': 'rgb(120 120 120)',
+            },
+        },
+    },
+    plugins: [],
+}
