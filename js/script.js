eb2f1be9b20c9e56eb5ac161590db287eb2cfb35
katebmedia <katebmedia@gmail.com>
2024-02-13 20:41:13 +0330

    feat: :sparkles: home page in desktop mode

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
-var swiper = new Swiper(".swiper-clients", {
+var swiperClients = new Swiper(".swiper-clients", {
       spaceBetween: 30,
       grabCursor: true,
       slidesPerView: "auto",
       loop: true,
       autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+            delay: 2500,
+            disableOnInteraction: false,
       },
 });
-var swiper = new Swiper(".swiper-selling", {
+var swiperSelling = new Swiper(".swiper-selling", {
       spaceBetween: 24,
       grabCursor: true,
       slidesPerView: 3,
       loop: true,
       autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+            delay: 3000,
+            disableOnInteraction: false,
       },
+});
+var swiperComments = new Swiper(".swiper-comments", {
+      effect: "cards",
+      slidesPerView: "auto",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      cardsEffect: {
+        perSlideOffset: 15, // Space between cards in px
+        perSlideRotate: 0, // Rotation of cards in degrees
+      },
+      loop: true,
+      // autoplay: {
+      //       delay: 2700,
+      //       disableOnInteraction: false,
+      // },
 });
